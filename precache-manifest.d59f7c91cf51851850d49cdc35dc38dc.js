381979a8869f95726788467bd0e6a099c8300821
Hudson Menezes <hudson.bm23@gmail.com>
2022-06-17 15:47:03 -0300

    Updates

---
+++ precache-manifest.d59f7c91cf51851850d49cdc35dc38dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5feed773aa6fe4d3a0a",
+    "url": "/todolist-react/static/css/main.722eea8d.chunk.css"
+  },
+  {
+    "revision": "f5feed773aa6fe4d3a0a",
+    "url": "/todolist-react/static/js/main.f5feed77.chunk.js"
+  },
+  {
+    "revision": "401ab0ebf1c301fc385c",
+    "url": "/todolist-react/static/css/1.0cc0a294.chunk.css"
+  },
+  {
+    "revision": "401ab0ebf1c301fc385c",
+    "url": "/todolist-react/static/js/1.401ab0eb.chunk.js"
+  },
+  {
+    "revision": "d39cff1a79defb0fe09c",
+    "url": "/todolist-react/static/js/runtime~main.d39cff1a.js"
+  },
+  {
+    "revision": "64bfee79238d137005a23fc852cc6299",
+    "url": "/todolist-react/index.html"
+  }
+];
