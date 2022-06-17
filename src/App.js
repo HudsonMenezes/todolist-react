3f0fc393fd3dbbf436ba159e8d57394cc79dc3bc
Hudson Menezes <hudson.bm23@gmail.com>
2022-06-17 09:07:16 -0300

    added styles and creating tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { IoMdAdd } from 'react-icons/io'
+import './styles.css'
+import './global.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo">
+        {/* Aqui fica o Input */}
+
+        <header>
+          <input type="text" placeholder="Adicione uma tarefa" />
+          <button>
+            <IoMdAdd />
+          </button>
+        </header>
+
+        {/* Aqui começa a seção das tasks */}
+        <div className="task-container">
+          {/* Começa a task */}
+          <div className="task">
+            <label className="checkbox-container">
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <p>Criar Site</p>
+          </div>
+          {/* Termina a task */}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
